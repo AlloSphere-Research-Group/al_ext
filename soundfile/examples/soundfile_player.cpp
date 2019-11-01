b391a7e52882784d3ac2f66f0080a420ab229494
Andres Cabrera <andres@office>
2019-11-01 12:17:33 -0700

    Fixed exmaples for changes in allolib

--- soundfile/examples/soundfile_player.cpp
+++ soundfile/examples/soundfile_player.cpp
@@ -1,38 +1,54 @@
 #include <memory>
 
-#include "al/core/app/al_App.hpp"
-
+#include "al/app/al_App.hpp"
 #include "al_ext/soundfile/al_SoundfileBuffered.hpp"
 
 using namespace al;
 
+// This example shows usage of the SoundFileBuffered class
+// This class should be used when you need to stream a soundfile
+// from disk in a flexible manner (with seek)
+
 struct MyApp : public App {
+  // SoundFileBuffered object
+  SoundFileBuffered soundFile;
+  // Buffer to read samples into
+  std::unique_ptr<float[]> buffer;
 
-    SoundFileBuffered soundFile;
-    std::unique_ptr<float []> buffer;
-
-    void onInit() {
-        soundFile.open("data/count.wav");
-        buffer = std::make_unique<float []>(audioIO().framesPerBuffer() * soundFile.channels());
+  void onInit() {
+    // Open sound file. Point this to an available soundfile in your system
+    if (!soundFile.open("data/count.wav")) {
+      std::cout << "**** ERROR: Soundfile not found. Quitting." << std::endl;
+      quit();
+      return;
     }
 
-    void onSound(AudioIOData &io) override {
-        // The sound file can copy directly to the output buffer
-        soundFile.read(buffer.get(), io.framesPerBuffer());
-        float *sfBuffer = buffer.get();
-        while(io()) {
-            io.out(0) = *sfBuffer;
-            // If soundfile had two channels, you could read it with:
-//            io.out(1) = *(sfBuffer + 1);
-            sfBuffer += soundFile.channels(); // advance pointer to next audio frame
-        }
+    // Instantiate buffer according to number of channels in file and frames per
+    // buffer for sound card
+    buffer = std::make_unique<float[]>(audioIO().framesPerBuffer() *
+                                       soundFile.channels());
+  }
+
+  void onSound(AudioIOData &io) override {
+    // Read from sound file into buffer
+    soundFile.read(buffer.get(), io.framesPerBuffer());
+    float *sfBuffer = buffer.get();
+    while (io()) {
+      io.out(0) = *sfBuffer;
+      // If soundfile had two channels, you could read it with:
+      //            io.out(1) = *(sfBuffer + 1);
+      sfBuffer += soundFile.channels();  // advance pointer to next audio frame
     }
 
+    // Alternatively to this, the sound file object can copy directly to the
+    // output buffer if it is mono. This will overwrite anything already in the
+    // buffer, though...
+    //      soundFile.read(io.outBuffer(0), io.framesPerBuffer());
+  }
 };
 
 int main(int argc, char *argv[]) {
-    MyApp app;
-    app.initAudio();
-    app.start();
-    return 0;
+  MyApp app;
+  app.start();
+  return 0;
 }
